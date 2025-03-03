301e83b79adbc34871d6668a05a439566b9c0c37
joiceron <joiceron@hotmail.com>
2025-03-03 17:31:01 -0400

    Deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from '/Logo.png'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +12,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
