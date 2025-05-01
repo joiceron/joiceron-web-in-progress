ddb95c516284514990c22624c8775265ddd4f3ae
joiceron <joiceron@hotmail.com>
2025-04-30 23:56:59 -0300

    BrowserRouter in github pages test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import AboutMe from "./components/AboutMe/AboutMe";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/AboutMe" element={<AboutMe />} />
-          {/* <Route path="*" element={<Hero />} /> */}
+          <Route path="*" element={<Hero />} />
         </Routes>
         <Footer />
       </Router>
