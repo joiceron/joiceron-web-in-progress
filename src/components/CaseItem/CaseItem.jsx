3acb04d497fa61c3701a1b770ec540757981fce2
joiceron <joiceron@hotmail.com>
2025-05-29 00:32:55 -0300

    Add information in ui-ux section and fix some styling

--- src/components/CaseItem/CaseItem.jsx
+++ src/components/CaseItem/CaseItem.jsx
@@ -8,7 +8,6 @@ import sketch02 from "/case-study-images/sketch02.png";
 import sketch04 from "/case-study-images/sketch04.jpg";
 import sketch05 from "/case-study-images/sketch05.jpg";
 import sketch06 from "/case-study-images/sketch06.jpg";
-import linkIcon from "../../assets/icons/link-icon.svg";
 
 export default function CaseItem() {
   return (
@@ -27,27 +26,21 @@ export default function CaseItem() {
           <h3 className="">Overview</h3>
           <p className="">{studyCases[0].description}</p>
 
-          <div className="divider">
-            <div className="divider__container">
-              <h3 className="">Problem Space</h3>
-              {studyCases[0].problem.map((item, index) => (
-                <li className="" key={index}>
-                  {item}
-                </li>
-              ))}
-            </div>
-            <div className="divider__container">
-              <h3>Solutions</h3>
-              {studyCases[0].solutions.map((item, index) => (
-                <li className="" key={index}>
-                  {item}
-                </li>
-              ))}
-            </div>
-          </div>
+          <h3 className="">Problem Space</h3>
+          {studyCases[0].problem.map((item, index) => (
+            <li className="list" key={index}>
+              {item}
+            </li>
+          ))}
+          <h3>Solutions</h3>
+          {studyCases[0].solutions.map((item, index) => (
+            <li className="list" key={index}>
+              {item}
+            </li>
+          ))}
 
           <h2 className="emphasis">Design Process</h2>
-          <ol className="divider bold">
+          <ol className="case__text--list divider bold ">
             {studyCases[0].process.map((item, index) => (
               <li className="case__text--tag" key={index}>
                 {item}
@@ -56,10 +49,9 @@ export default function CaseItem() {
           </ol>
         </div>
         <h3 className="">01 Empathize</h3>
+        <p className="bold">Goal</p>
         <p>
-          The application is designed for individual users, not businesses. It
-          targets young adults looking to improve their financial control. It is
-          aimed at people who prefer cute and visual-candy interfaces.
+          Research and understand the user's needs, problems, and behaviors.
         </p>
         <p className="bold">Research</p>
         <p>
@@ -67,9 +59,8 @@ export default function CaseItem() {
           questions:
         </p>
         <ol>
-          {" "}
           {studyCases[0].questions.map((item, index) => (
-            <li className="" key={index}>
+            <li className="list" key={index}>
               {item}
             </li>
           ))}
@@ -81,11 +72,19 @@ export default function CaseItem() {
           want to know how their finances will be at the end of the month or
           year. Visually boring. Current tools are uninspiring or too "serious."
         </p>
-        {/* <p>Question made</p>
-        <p>quantitative research (graficos)</p> */}
         <p className="bold">Competitive Analysis</p>
         <p> {studyCases[0].comparative} </p>
         <h3 className="">02 Define</h3>
+        <p>
+          By analyzing user research and findings, we can clearly define the
+          core problems that need to be solved. Creating a user persona helps
+          visualize and empathize with real users their needs, goals, pain
+          points, and behaviors. This understanding allows us to make informed
+          design decisions that align with user expectations and improve
+          usability. Throughout the process, we can evaluate ideas by asking,
+          “Would this help Anna?” ensuring that each feature remains relevant,
+          purposeful, and user-centered.
+        </p>
         <p className="bold">User Persona</p>
         <img
           className="case__multimedia--chart"
@@ -93,6 +92,14 @@ export default function CaseItem() {
           alt="User persona chart"
         />
         <h3 className="">03 Ideate</h3>
+        <p>
+          In the Ideate phase, the goal is to brainstorm and explore creative
+          solutions focused on improving the user experience. With a clear
+          understanding of user needs and pain points, a flow chart was
+          generated to reflect the simple and intuitive app they require. Every
+          idea is evaluated through the lens of our user persona to ensure that
+          the final design offers both functionality and delight.
+        </p>
         <p className="bold"> Flow chart</p>
         <img
           className="case__multimedia--chart"
@@ -100,30 +107,54 @@ export default function CaseItem() {
           alt="Flow chart"
         />
         <h3 className="">04 Design</h3>
-        <p className="bold">Sketchs</p>
+        <p>
+          Prototypes were created to bring the solution to life and visualize
+          the app’s structure and flow.
+        </p>
+        <p>
+          The initial designs began as pencil sketches to quickly explore layout
+          ideas and user interface elements.
+        </p>
         <img
           className="case__multimedia--chart"
           src={sketch01}
           alt="Design chart"
         />
+        <p>
+          These low fidelity wireframes helped establish the content hierarchy
+          and navigation of the single-page application.
+        </p>
+        <img
+          className="case__multimedia--chart"
+          src={sketch02}
+          alt="Design chart"
+        />
+        <p>
+          The visual identity of BudgetBloom uses a soft, approachable palette
+          to make financial planning feel less intimidating:
+        </p>
         <img
           className="case__multimedia--chart"
           src={sketch04}
           alt="Design chart"
         />
+        <p>
+          This logo represents the emotional side of budgeting helping users not
+          only control their money but also see it flourish.
+        </p>
         <img
           className="case__multimedia--chart"
           src={sketch05}
           alt="Design chart"
         />
+        <p>
+          The app uses Bungee for titles and headings to give a modern, rounded
+          look, and Montserrat for body text to ensure readability and
+          neutrality.
+        </p>
         <img
           className="case__multimedia--chart"
           src={sketch06}
-          alt="Design chart"
-        />
-        <img
-          className="case__multimedia--chart"
-          src={sketch02}
           alt="Design chart"
         />
       </div>
